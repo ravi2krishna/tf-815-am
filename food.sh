33ef856d80e546a38e94108b2d9fc110f205e9ef
Ravi <ravi2krishna@gmail.com>
2023-12-08 08:48:25 +0530

    Update and rename ecomm.sh to food.sh

--- food.sh
+++ food.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install -y nginx
 sudo rm /var/www/html/*.html
-sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
+sudo git clone https://github.com/ravi2krishna/food.git /var/www/html
