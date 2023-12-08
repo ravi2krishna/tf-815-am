2728e1b434ee1884719380f9917bab864f37ceea
Ravi <ravi2krishna@gmail.com>
2023-12-08 08:48:56 +0530

    Update aws_resources.tf

--- aws_resources.tf
+++ aws_resources.tf
@@ -90,9 +90,9 @@ resource "aws_instance" "tf-ec2" {
   key_name = "ravi"
   subnet_id = aws_subnet.tf-subnet.id
   vpc_security_group_ids = [aws_security_group.tf-sg.id]
-  user_data = file("ecomm.sh")
+  user_data = file("food.sh")
 
   tags = {
-    Name = "ecomm-server"
+    Name = "food-server"
   }
 }
